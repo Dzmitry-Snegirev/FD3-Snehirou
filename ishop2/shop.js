35b3969367c675d91d714873c716b4c82a9f4faf
John Doe <whitesurfer.89@gmail.com>
2022-04-30 19:30:41 +0300

    ishop2

--- ishop2/shop.js
+++ ishop2/shop.js
@@ -1,63 +1,28 @@
 var Shop = React.createClass({
 
-	displayName: 'Shop',
-	/*
-		propTypes: {
-			names: React.PropTypes.array.isRequired,
-			catalog: React.PropTypes.array.isRequired,
-		},
-	*/
-	getInitialState: function () {
-		return {
-			selectedLineCode: null,
-		};
-	},
+    displayName: 'Shop',
+    /*
+    	propTypes: {
+    		names: React.PropTypes.array.isRequired,
+    		catalog: React.PropTypes.array.isRequired,
+    	},
+    */
+    getInitialState: function() {
+        return {
+            selectedLineCode: null,
+        };
+    },
+    selected: function(code) {
+        this.setState({ selectedLineCode: code });
+    },
 
-	render: function () {
-		var catalogNamesCodes = [];
-		this.props.names.forEach(v => catalogNamesCodes.push(React.DOM.th({ key: v.code, className: 'itemName' }, v.name)));
-		return React.DOM.table({ className: 'tableShop' },
-			React.DOM.thead({ className: 'names' }, React.DOM.tr({ className: 'CatalogNames' }, catalogNamesCodes)),
-			React.createElement(ProductsLine, { catalog: catalogItemsArr, }),
-		);
-	},
+    render: function() {
+        var catalogNamesCodes = [];
+        this.props.names.forEach(v => catalogNamesCodes.push(React.DOM.th({ key: v.code, className: 'itemName', }, v.name)));
+        return React.DOM.table({ className: 'tableShop' },
+            React.DOM.thead({ className: 'names' }, React.DOM.tr({ className: 'CatalogNames' }, catalogNamesCodes)),
+            React.createElement(ProductLine, { pack: this.props.items, cbSelected: this.selected }),
+        );
+    },
 
 });
-
-
-/*
-			var catalogNamesCodes = [];
-			for (var a = 0; a < this.props.names.length; a++) {
-				var itemName = this.props.names[a];
-				var catalogNamesCode =
-					React.DOM.tr({ key: itemName.code, className: 'CatalogNames' },
-						React.DOM.th({ className: 'NameItem' }, itemName),
-						React.DOM.th({ className: 'NameItem' }, itemName),
-						React.DOM.th({ className: 'NameItem' }, itemName),
-						React.DOM.th({ className: 'NameItem' }, itemName),
-					);
-				catalogNamesCodes.push(catalogNamesCode);
-			}*/
-/*
-		var catalogItemsCodes = [];
-	
-		this.props.catalog.forEach(l => catalogItemsCodes.push(React.DOM.tr({ key: l.code, className: 'info' },
-			React.DOM.td({ className: 'Text' }, l.text),
-			React.DOM.td({ className: 'Count' }, l.count),
-			React.DOM.td({ className: 'Text' }, l.price),
-			React.DOM.td({ className: 'Foto' }, React.DOM.img({ className: 'FotoItem', src: l.foto })),
-		)));
-*/
-/*
-		for (var i = 0; i < this.props.catalog.length; i++) {
-			var catalogItem = this.props.catalog[i];
-			var catalogItemCode =
-				React.DOM.tr({ key: catalogItem.code, className: 'info' },
-					React.DOM.td({ className: 'Text' }, catalogItem.text),
-					React.DOM.td({ className: 'Count' }, catalogItem.count),
-					React.DOM.td({ className: 'Text' }, catalogItem.price),
-					React.DOM.td({ className: 'Foto' }, React.DOM.img({ className: 'FotoItem', src: catalogItem.foto },)),
-				);
-			catalogItemsCodes.push(catalogItemCode);
-		}
-*/
