f9cd7407fc5728c7faf7c7e2f70eee517a83f42b
John Doe <whitesurfer.89@gmail.com>
2022-05-01 19:29:35 +0300

    ishop2

--- ishop2/product.js
+++ ishop2/product.js
@@ -1,30 +1,41 @@
 var ProductLine = React.createClass({
 
-	displayName: 'ProductLine',
-	/*
-		propTypes: {
-			names: React.PropTypes.array.isRequired,
-			catalog: React.PropTypes.array.isRequired,
-		},
-	*/
+    displayName: 'ProductLine',
+    /*
+    	propTypes: {
+    		names: React.PropTypes.array.isRequired,
+    		catalog: React.PropTypes.array.isRequired,
+    	},
+    */
 
-	selectLine: function (code) {
-		this.props.cbSelected(code);
-	},
+    selectLine: function(code) {
+        this.props.cbSelected(code);
+    },
+    del: function(eo) {
+        eo.stopPropagation();
+        this.props.cbdel(this.props.code);
+    },
 
 
-	render: function () {
-		var catalogItemsCodes = [];
-		this.props.pack.forEach(l => catalogItemsCodes.push(React.DOM.tr({
-			key: l.code, className: 'info', onClick: () => this.selectLine(l.code),
-			style: { backgroundColor: (this.props.startCode === l.code) ? 'orange' : 'white' }
-		},
-			React.DOM.td({ className: 'Text' }, l.text,),
-			React.DOM.td({ className: 'Count' }, l.count,),
-			React.DOM.td({ className: 'Text' }, l.price,),
-			React.DOM.td({ className: 'Foto' }, React.DOM.img({ className: 'FotoItem', src: l.foto })),
-			React.DOM.td({ className: 'button' }, React.DOM.input({ className: 'reset', type: "button", value: "Удалить", })),
-		)));
-		return React.DOM.tbody({ className: 'items' }, catalogItemsCodes);
-	},
+
+
+    render: function() {
+
+
+        var catalogItemsCodes = [];
+        this.props.pack.forEach(l => catalogItemsCodes.push(React.DOM.tr({
+                key: l.code,
+                className: 'info',
+                onClick: () => this.selectLine(l.code),
+                style: { backgroundColor: (this.props.startCode === l.code) ? 'orange' : 'white' }
+            },
+            React.DOM.td({ className: 'Text' }, l.text, ),
+            React.DOM.td({ className: 'Count' }, l.count, ),
+            React.DOM.td({ className: 'Text' }, l.price, ),
+            React.DOM.td({ className: 'Foto' }, React.DOM.img({ className: 'FotoItem', src: l.foto })),
+            React.DOM.td({ className: 'button' }, React.DOM.input({ className: 'reset', type: "button", value: "Удалить", onClick: this.del })),
+        )));
+
+        return React.DOM.tbody({ className: 'items' }, catalogItemsCodes);
+    },
 });
--- ishop2/shop.js
+++ ishop2/shop.js
@@ -1,28 +1,35 @@
 var Shop = React.createClass({
 
-	displayName: 'Shop',
-	/*
-		propTypes: {
-			names: React.PropTypes.array.isRequired,
-			catalog: React.PropTypes.array.isRequired,
-		},
-	*/
-	getInitialState: function () {
-		return {
-			selectedLineCode: null,
-		};
-	},
-	selected: function (key) {
-		this.setState({ selectedLineCode: key });
-	},
+    displayName: 'Shop',
+    /*
+    	propTypes: {
+    		names: React.PropTypes.array.isRequired,
+    		catalog: React.PropTypes.array.isRequired,
+    	},
+    */
+    getInitialState: function() {
+        return {
+            selectedLineCode: null,
+            startPack: this.props.items,
+        };
+    },
+    selected: function(key) {
+        this.setState({ selectedLineCode: key });
+    },
+    del: function(code) {
+        var copyStartPack = this.state.startPack.slice();
+        copyStartPack.splice(code, 1);
+        this.setState({ startPack: copyStartPack });
+    },
 
-	render: function () {
-		var catalogNamesCodes = [];
-		this.props.names.forEach(v => catalogNamesCodes.push(React.DOM.th({ key: v.code, className: 'itemName', }, v.name)));
-		return React.DOM.table({ className: 'tableShop' },
-			React.DOM.thead({ className: 'names' }, React.DOM.tr({ className: 'CatalogNames' }, catalogNamesCodes)),
-			React.createElement(ProductLine, { pack: this.props.items, cbSelected: this.selected, startCode: this.state.selectedLineCode }),
-		);
-	},
 
+
+    render: function() {
+        var catalogNamesCodes = [];
+        this.props.names.forEach(v => catalogNamesCodes.push(React.DOM.th({ key: v.code, className: 'itemName', }, v.name)));
+        return React.DOM.table({ className: 'tableShop' },
+            React.DOM.thead({ className: 'names' }, React.DOM.tr({ className: 'CatalogNames' }, catalogNamesCodes)),
+            React.createElement(ProductLine, { pack: this.state.startPack, cbSelected: this.selected, startCode: this.state.selectedLineCode, cbdel: this.del }),
+        );
+    },
 });
