49dda0f54d3d05fc5dbfc8ec375ee7468346d068
Dzmitry-Snegirev <snegirev.dzmitry@gmail.com>
2022-05-01 06:57:33 +0300

    orange line

--- ishop2/shop.js
+++ ishop2/shop.js
@@ -1,28 +1,28 @@
 var Shop = React.createClass({
 
-    displayName: 'Shop',
-    /*
-    	propTypes: {
-    		names: React.PropTypes.array.isRequired,
-    		catalog: React.PropTypes.array.isRequired,
-    	},
-    */
-    getInitialState: function() {
-        return {
-            selectedLineCode: null,
-        };
-    },
-    selected: function(code) {
-        this.setState({ selectedLineCode: code });
-    },
+	displayName: 'Shop',
+	/*
+		propTypes: {
+			names: React.PropTypes.array.isRequired,
+			catalog: React.PropTypes.array.isRequired,
+		},
+	*/
+	getInitialState: function () {
+		return {
+			selectedLineCode: null,
+		};
+	},
+	selected: function (key) {
+		this.setState({ selectedLineCode: key });
+	},
 
-    render: function() {
-        var catalogNamesCodes = [];
-        this.props.names.forEach(v => catalogNamesCodes.push(React.DOM.th({ key: v.code, className: 'itemName', }, v.name)));
-        return React.DOM.table({ className: 'tableShop' },
-            React.DOM.thead({ className: 'names' }, React.DOM.tr({ className: 'CatalogNames' }, catalogNamesCodes)),
-            React.createElement(ProductLine, { pack: this.props.items, cbSelected: this.selected }),
-        );
-    },
+	render: function () {
+		var catalogNamesCodes = [];
+		this.props.names.forEach(v => catalogNamesCodes.push(React.DOM.th({ key: v.code, className: 'itemName', }, v.name)));
+		return React.DOM.table({ className: 'tableShop' },
+			React.DOM.thead({ className: 'names' }, React.DOM.tr({ className: 'CatalogNames' }, catalogNamesCodes)),
+			React.createElement(ProductLine, { pack: this.props.items, cbSelected: this.selected, startCode: this.state.selectedLineCode }),
+		);
+	},
 
 });
