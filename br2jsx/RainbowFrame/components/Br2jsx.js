cffd8c90dc44bb653dd664d329e915013b6ff4fc
Dzmitry-Snegirev <snegirev.dzmitry@gmail.com>
2022-05-23 00:24:40 +0300

    Br2jsx

---
+++ br2jsx/RainbowFrame/components/Br2jsx.js
@@ -0,0 +1,18 @@
+﻿import React from 'react';
+
+
+class Br2jsx extends React.Component {
+	render() {
+		const words = this.props.text.split(/<br\s*\/?>/);
+		const lines = [];
+		words.forEach((element, i) => {
+			if (i)
+				lines.push(<br />)
+			lines.push(element)
+		});
+		return <div style={{ color: "white", fontSize: "2em" }}>{lines}</div>;
+	}
+
+};
+
+export default Br2jsx;
