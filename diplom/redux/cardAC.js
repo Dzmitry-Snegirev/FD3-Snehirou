178a05138ba19a863ab5b87d4f38bcb593bf5293
Dzmitry-Snegirev <snegirev.dzmitry@gmail.com>
2022-06-10 00:12:49 +0300

    basket

--- diplom/redux/cardAC.js
+++ diplom/redux/cardAC.js
@@ -1,39 +1,13 @@
-const initialState = {
-	items: [{
-		"id": 0,
-		"title": "Происхождение",
-		"author": "Дэн Браун",
-		"image": "https://cv9.litres.ru/pub/c/elektronnaya-kniga/cover_415/27624091-den-braun-proishozhdenie-27624091.jpg",
-		"price": 710,
-		"rating": 3
-	},
-	{
-		"id": 0,
-		"title": "Происхождение",
-		"author": "Дэн Браун",
-		"image": "https://cv9.litres.ru/pub/c/elektronnaya-kniga/cover_415/27624091-den-braun-proishozhdenie-27624091.jpg",
-		"price": 710,
-		"rating": 3
-	},
-	{
-		"id": 0,
-		"title": "Происхождение",
-		"author": "Дэн Браун",
-		"image": "https://cv9.litres.ru/pub/c/elektronnaya-kniga/cover_415/27624091-den-braun-proishozhdenie-27624091.jpg",
-		"price": 710,
-		"rating": 3
-	}]
-};
 
 const ADD_TO_CART = 'ADD_TO_CART';
 const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
 
 
 
-const addToCart = function (books) {
+const addToCart = function (obj) {
 	return {
 		type: ADD_TO_CART,
-		cardBook: books,
+		cardBook: obj,
 	};
 }
 
